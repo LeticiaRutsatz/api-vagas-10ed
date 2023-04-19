2156d84cf784b3124fa5175da91351c48c976319
Anderson Wilmsen <ander.wilmsen98@gmail.com>
2023-04-18 22:41:43 -0300

    Teste

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,4 @@ import 'dotenv/config';
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log('Server is running... in port ' + port));
+app.listen(port, () => console.log('TESTE ' + port));
