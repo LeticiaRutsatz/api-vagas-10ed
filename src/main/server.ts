67835e2fac634b5c0b723e6490d54aca62d046b9
Anderson Wilmsen <ander.wilmsen98@gmail.com>
2023-04-18 22:46:17 -0300

    Ajustado listen e import server

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,6 @@
-import app from './main/config/app';
+import app from './config/app';
 import 'dotenv/config';
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log('TESTE ' + port));
+app.listen(port, () => console.log('Server is running ' + port));
