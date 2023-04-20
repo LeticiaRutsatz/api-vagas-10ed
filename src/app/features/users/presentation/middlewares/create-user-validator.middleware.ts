75f2ba7204eb3913435d1ebddc593a3b6efeadd8
Edson Martins <edsonmartins3110@gmail.com>
2023-04-19 23:29:19 -0300

    ajusta substituicao do valor do body

--- src/app/features/users/presentation/middlewares/create-user-validator.middleware.ts
+++ src/app/features/users/presentation/middlewares/create-user-validator.middleware.ts
@@ -1,36 +1,28 @@
-import { NextFunction, Request, Response } from "express";
-import { z, ZodError } from "zod";
-import { Profile } from "../../../../shared/domain/enums";
-
+import { NextFunction, Request, Response } from 'express';
+import { z, ZodError } from 'zod';
+import { Profile } from '../../../../shared/domain/enums';
 
 export const createUserValidator = (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.body);
-  
-  const scheme = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    profile: z.nativeEnum(Profile)
-  })
+    const scheme = z.object({
+        name: z.string().min(2),
+        email: z.string().email(),
+        profile: z.nativeEnum(Profile),
+    });
 
-  try {
-
-    const data = scheme.parse(req.body)
-    req.body = data
-    return next()
-
-  } catch (error: any) {
-    
-    if (error instanceof ZodError) {
-      return res.status(400).json(
-        {
-          error: error.issues.map((issue) => ({
-            campo: issue.path[0],
-            mensagem: issue.message,
-            codigo: issue.code,
-          }))
+    try {
+        const data = scheme.parse(req.body);
+        Object.assign(req.body, data);
+        return next();
+    } catch (error: any) {
+        if (error instanceof ZodError) {
+            return res.status(400).json({
+                error: error.issues.map((issue) => ({
+                    campo: issue.path[0],
+                    mensagem: issue.message,
+                    codigo: issue.code,
+                })),
+            });
         }
-      );
+        throw error;
     }
-    throw error;
-  }
-}
+};
