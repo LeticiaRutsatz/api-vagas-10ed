8e96e88a76d2a103dbcf7b7e412ec11d69263892
Edson Martins <edsonmartins3110@gmail.com>
2023-04-19 23:29:40 -0300

    formata no estilo

--- src/app/features/users/presentation/controllers/user.controller.ts
+++ src/app/features/users/presentation/controllers/user.controller.ts
@@ -1,8 +1,8 @@
-import { Request, Response, response } from "express";
+import { Request, Response } from 'express';
 
-export class UserController{
-    createUser(req: Request, res: Response){
-        return res.status(200).json('ok')
+export class UserController {
+    createUser(req: Request, res: Response) {
+        console.log(req.body);
+        return res.status(200).json('ok');
     }
-
 }
