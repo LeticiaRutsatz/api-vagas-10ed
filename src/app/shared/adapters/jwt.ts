7ae9948dc8b0f0e1cbd703aefab9fc4fc72955b0
Edson Martins <edsonmartins3110@gmail.com>
2023-04-26 20:04:16 -0300

    feat: adiciona camada de configuracao

--- src/app/shared/adapters/jwt.ts
+++ src/app/shared/adapters/jwt.ts
@@ -1,19 +1,17 @@
 import jwt from 'jsonwebtoken';
-import 'dotenv/config';
+import { jwtConfig } from '../../../config/jwt.config';
 
 interface Token {
     sign(data: any): string;
     verify(token: string): string | jwt.JwtPayload;
 }
 
-const jwtKey = process.env.JWT_KEY as string;
-
 export class JwtToken implements Token {
     sign(data: any): string {
-        return jwt.sign(data, jwtKey, { expiresIn: '1h' });
+        return jwt.sign(data, jwtConfig.key, { expiresIn: jwtConfig.expireIn });
     }
 
     verify(token: string): string | jwt.JwtPayload {
-        return jwt.verify(token, jwtKey, { maxAge: '1h' });
+        return jwt.verify(token, jwtConfig.key, { maxAge: jwtConfig.maxAge });
     }
 }
