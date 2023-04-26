61152ef35df614cf8aa2699b4a6bf3d6300a7ad1
windsonamorim <windson.amorim@gmail.com>
2023-04-25 21:54:35 -0300

    feat: adiciona rota login na inicialização

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,7 +1,10 @@
 import express, { Express } from 'express';
 import userRoutes from '../../app/features/users/presentation/routes/user.routes';
+import loginRoutes from '../../app/features/authentication/presentation/routes/auth.routes';
+
 
 export default (app: Express) => {
     app.get('/', (req, res) => res.status(200).json('API is runnig...'));
     app.use(userRoutes());
+    app.use(loginRoutes())
 };
