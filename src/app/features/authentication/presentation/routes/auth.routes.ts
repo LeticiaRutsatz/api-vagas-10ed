534c467b9f9779cbe17a0ef54c9e828237c60227
windsonamorim <windson.amorim@gmail.com>
2023-04-25 21:52:26 -0300

    feat: cria rota login

---
+++ src/app/features/authentication/presentation/routes/auth.routes.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import { loginValidation } from '../middlewares';
+import { AuthenticationController } from '../controllers';
+
+export default () => {
+    const router = express.Router();
+
+    router.post('/login', loginValidation, AuthenticationController.login);
+
+    return router;
+};
