2b921c5b39f9033e090af8a28385b6890c6c4abc
Bruno Beise <bruno_beise@hotmail.com>
2023-04-26 22:30:52 -0300

    feat: adiciona validação ADMIN

--- src/app/features/users/presentation/controllers/user.controller.ts
+++ src/app/features/users/presentation/controllers/user.controller.ts
@@ -3,9 +3,15 @@ import { UserRepository } from '../../infra/repositories/user.repository';
 import { BCryptPassword } from '../../../../shared/adapters/crypto';
 import { badRequest, ok } from '../../../../shared/presentation/http-helper';
 import { UserSharedRepository } from '../../../../shared/infra/repositories';
+import { Profile } from '../../../../shared/domain/enums';
 
 export class UserController {
     async createUser(req: Request, res: Response) {
+        console.log(req.user);
+
+        if (req.user.profile !== Profile.ADMIN) {
+            return badRequest(res, { success: false, error: 'User is not ADMIN' });
+        }
         const repository = new UserRepository();
         const sharedRepository = new UserSharedRepository();
         const bcrypt = new BCryptPassword();
