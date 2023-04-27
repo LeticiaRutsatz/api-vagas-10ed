f5d228ccc5533cdd4c0cb3e7788b9f50ecaf99dc
Bruno Beise <bruno_beise@hotmail.com>
2023-04-26 22:30:30 -0300

    feat: adiciona propriedade user no Request

--- src/app/shared/presentation/middlewares/auth.middleware.ts
+++ src/app/shared/presentation/middlewares/auth.middleware.ts
@@ -1,28 +1,32 @@
-import { NextFunction, Request, Response } from "express";
-import { unauthorized } from "../http-helper";
-import { JwtToken } from "../../adapters/jwt";
-import { TokenError } from "../../domain/errors";
+import { NextFunction, Request, Response } from 'express';
+import { unauthorized } from '../http-helper';
+import { JwtToken } from '../../adapters/jwt';
+import { TokenError } from '../../domain/errors';
 
 export const auth = (req: Request, res: Response, next: NextFunction) => {
-  const authorization  = req.headers.authorization;
+    const authorization = req.headers.authorization;
 
-  if(!authorization){
-    return unauthorized(res, {success: false, error:"invalid token"})
-  }
-
-  const token = authorization.split(" ")[1];
-
-  if(!token){
-    return unauthorized(res, {success: false, error:"invalid token"})
-  }
-
-  try {
-    const auth = new JwtToken().verify(token)
-    res.status(200).send("ok")
-  }catch(error){
-    if(error instanceof TokenError){
-      return unauthorized(res, {success: false, error:"invalid token"})
+    if (!authorization) {
+        return unauthorized(res, { success: false, error: 'invalid token' });
     }
 
-  }
-}
+    const token = authorization.split(' ')[1];
+
+    if (!token) {
+        return unauthorized(res, { success: false, error: 'invalid token' });
+    }
+
+    try {
+        const auth = new JwtToken().verify(token);
+        req.user = {
+            id: auth.id,
+            profile: auth.profile,
+        };
+        return next();
+    } catch (error) {
+        if (error instanceof TokenError) {
+            return unauthorized(res, { success: false, error: 'invalid token' });
+        }
+        throw error;
+    }
+};
