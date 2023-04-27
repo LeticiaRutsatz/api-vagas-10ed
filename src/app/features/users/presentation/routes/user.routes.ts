1d44740653b9759b2e58610998dfe3a4cc7e6a36
AndersonExner <andersonexner@gmail.com>
2023-04-26 22:05:13 -0300

    refactor: adiciona middleware de autentificação na rota

--- src/app/features/users/presentation/routes/user.routes.ts
+++ src/app/features/users/presentation/routes/user.routes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { UserController } from '../controllers';
 import { createUserValidator } from '../middlewares';
+import { auth } from '../../../../shared/presentation/middlewares';
 
 export default () => {
     const router = express.Router();
 
-    router.post('/users', createUserValidator, new UserController().createUser);
+    router.post('/users', auth, createUserValidator, new UserController().createUser);
 
     return router;
 };
