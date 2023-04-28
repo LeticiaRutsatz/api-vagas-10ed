3538f3d8b4776300d02dfc0c77af140c0d272a2a
windsonamorim <windson.amorim@gmail.com>
2023-04-27 22:42:44 -0300

    refactor: remove casos de uso da controller

--- src/app/features/authentication/presentation/controllers/authentication.controller.ts
+++ src/app/features/authentication/presentation/controllers/authentication.controller.ts
@@ -1,44 +1,23 @@
 import { Request, Response } from 'express';
-import { BCryptPassword } from '../../../../shared/adapters/crypto';
-import { UserSharedRepository } from '../../../../shared/infra/repositories';
 import { ok, unauthorized } from '../../../../shared/presentation/http-helper';
-import { JwtToken } from '../../../../shared/adapters/jwt';
+import { LoginUseCase } from '../../domain/usecases';
+import { CustomError } from '../../../../shared/errors';
 
 export class AuthenticationController {
     async login(req: Request, res: Response) {
-        const { email, password } = req.body;
-        const repository = new UserSharedRepository();
-        const bcrypt = new BCryptPassword();
-
-        const user = await repository.getUserByEmail(email, {
-            withPassword: true,
-        });
-
-        if (!user) {
-            return unauthorized(res, { success: false, error: 'Incorret email or password.' });
+        try {
+            const { email, password } = req.body;
+            const useCase = new LoginUseCase();
+            const data = await useCase.execute({ email, password });
+            return ok(res, {
+                success: true,
+                data,
+            });
+        } catch (error: any) {
+            if (error instanceof CustomError) {
+                return unauthorized(res, { success: false, error: error.message });
+            }
+            throw error;
         }
-
-        const correctPassword = await bcrypt.comparePassword(password, user.password as string);
-
-        if (!correctPassword) {
-            return unauthorized(res, { success: false, error: 'Incorret  a email or password.' });
-        }
-
-        const jwtToken = new JwtToken();
-
-        const token = jwtToken.sign({
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            profile: user.profile,
-        });
-
-        return ok(res, {
-            success: true,
-            data: {
-                token,
-                user: { id: user.id, name: user.name, email: user.email, profile: user.profile },
-            },
-        });
     }
 }
