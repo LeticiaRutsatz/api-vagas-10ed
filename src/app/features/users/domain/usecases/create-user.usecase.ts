f6d3bd72f5561f45c07722c8d8a34a7e473e38b6
Gabriel Pacheco Passos da Silva <Pacheco_gbr@hotmail.com>
2023-04-27 21:53:31 -0300

    refactor: refatora código para lançar erros

--- src/app/features/users/domain/usecases/create-user.usecase.ts
+++ src/app/features/users/domain/usecases/create-user.usecase.ts
@@ -1,26 +1,31 @@
-import { BCryptPassword } from "../../../../shared/adapters/crypto";
-import { UserDetailDTO } from "../../../../shared/domain/dtos";
-import { UserSharedRepository } from "../../../../shared/infra/repositories";
-import { UserRepository } from "../../infra/repositories";
-import { CreateUserDTO } from "../dtos";
+import { profile } from 'console';
+import { BCryptPassword } from '../../../../shared/adapters/crypto';
+import { UserDetailDTO } from '../../../../shared/domain/dtos';
+import { UserSharedRepository } from '../../../../shared/infra/repositories';
+import { UserRepository } from '../../infra/repositories';
+import { CreateUserDTO } from '../dtos';
+import { Profile } from '../../../../shared/domain/enums';
+import { CustomError } from '../../../../shared/errors';
 
 export class CreateUserUseCase {
-      
-  async execute(createUserDTO: CreateUserDTO): Promise<UserDetailDTO | null>{
-    const repository = new UserRepository();
-    const sharedRepository = new UserSharedRepository();  
-    const exists = await sharedRepository.getUserByEmail(createUserDTO.email);
+    async execute(createUserDTO: CreateUserDTO): Promise<UserDetailDTO> {
+        if (createUserDTO.userTryingToCreateAnotherProfile !== Profile.ADMIN) {
+            throw new CustomError('User is not ADMIN');
+        }
+        const repository = new UserRepository();
+        const sharedRepository = new UserSharedRepository();
+        const exists = await sharedRepository.getUserByEmail(createUserDTO.email);
 
-    if (exists) return null
+        if (exists) throw new CustomError('Email already exists');
 
-    const bcrypt = new BCryptPassword();
+        const bcrypt = new BCryptPassword();
 
-    const hashPassword = await bcrypt.hashPassword(createUserDTO.password);
-    
-    const dto = Object.assign(createUserDTO, {password: hashPassword})
-    
-    const user = await repository.saveUser(dto);
+        const hashPassword = await bcrypt.hashPassword(createUserDTO.password);
 
-    return user;
-  }
+        const dto = Object.assign(createUserDTO, { password: hashPassword });
+
+        const user = await repository.saveUser(dto);
+
+        return user;
+    }
 }
