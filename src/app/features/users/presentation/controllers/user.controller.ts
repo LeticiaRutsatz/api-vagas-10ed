659faab861f06a3af06b35be2cc171bcbdfec658
Gabriel Pacheco Passos da Silva <Pacheco_gbr@hotmail.com>
2023-04-27 21:52:07 -0300

    refactor: encapsula caso de uso para preparar tratamento de erros

--- src/app/features/users/presentation/controllers/user.controller.ts
+++ src/app/features/users/presentation/controllers/user.controller.ts
@@ -4,18 +4,24 @@ import { badRequest, ok } from '../../../../shared/presentation/http-helper';
 import { UserSharedRepository } from '../../../../shared/infra/repositories';
 import { Profile } from '../../../../shared/domain/enums';
 import { CreateUserUseCase } from '../../domain/usecases/create-user.usecase';
+import { CustomError } from '../../../../shared/errors';
 
 export class UserController {
     async createUser(req: Request, res: Response) {
         const { name, email, profile, company } = req.body;
+        const userTryingToCreateAnotherProfile = req.user.profile;
         const password = process.env.ADMIN_PASSWORD!;
-        const useCase = new CreateUserUseCase();
-        const user = await useCase.execute({name,email,profile,company, password});
+        try {
+            const useCase = new CreateUserUseCase();
+            const user = await useCase.execute({ name, email, profile, company, password, userTryingToCreateAnotherProfile });
 
-        if(!user){
-            return badRequest(res, { success: false, error: 'Email already exists.' });
+            return ok(res, { success: true, data: user });
+        } catch (error: any) {
+            if (error instanceof CustomError) {
+                return badRequest(res, { success: false, error: error.message });
+            }
+
+            throw error;
         }
-
-        return ok(res, { success: true, data: user });
     }
 }
