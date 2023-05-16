e91e51ae49e4808a47641d64840720bda6c22fa2
Anderson Wilmsen <ander.wilmsen98@gmail.com>
2023-05-15 21:51:12 -0300

    feat: add teste de listar usuários.

---
+++ tests/app/features/users/domain/usecase/list-users.usecase.spec.ts
@@ -0,0 +1,31 @@
+import { ListUsersUseCase } from '../../../../../../src/app/features/users/domain/usecases/list-users.usecase';
+import { UserDetailDTO } from '../../../../../../src/app/shared/domain/dtos';
+import { Profile } from '../../../../../../src/app/shared/domain/enums';
+import { UserSharedRepository } from '../../../../../../src/app/shared/infra/repositories';
+
+describe('[Use Case] - List Users', () => {
+    it('List all users with 0 user.', async () => {
+        jest.spyOn(UserSharedRepository.prototype, 'getUserAll').mockResolvedValueOnce([]);
+        const sut = new ListUsersUseCase();
+
+        const users = await sut.execute();
+
+        expect(users).toHaveLength(0);
+    });
+
+    it('List all users with 1 user.', async () => {
+        const userDto: UserDetailDTO = {
+            id: 'any_id',
+            name: 'any_name',
+            email: 'any@email.com',
+            profile: Profile.ADMIN,
+        };
+
+        jest.spyOn(UserSharedRepository.prototype, 'getUserAll').mockResolvedValueOnce([userDto]);
+        const sut = new ListUsersUseCase();
+
+        const users = await sut.execute();
+
+        expect(users).toHaveLength(1);
+    });
+});
