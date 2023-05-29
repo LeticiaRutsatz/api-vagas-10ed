52008d0d2abf40d55550ae4c119733147570e0d4
LeticiaRutsatz <leticiarutsatz099@gmail.com>
2023-05-29 20:17:39 -0300

    Adição do Redis

--- src/app/features/users/domain/usecases/create-candidate.usecase.ts
+++ src/app/features/users/domain/usecases/create-candidate.usecase.ts
@@ -1,6 +1,6 @@
 import { BCryptPassword } from '../../../../shared/adapters/crypto';
 import { UserDetailDTO } from '../../../../shared/domain/dtos';
-import { UserSharedRepository } from '../../../../shared/infra/repositories';
+import { CacheRepository, UserSharedRepository } from '../../../../shared/infra/repositories';
 import { UserRepository } from '../../infra/repositories';
 import { CreateUserDTO } from '../dtos';
 import { CustomError } from '../../../../shared/errors';
@@ -9,6 +9,25 @@ export class CreateCandidateUseCase {
     async execute(createUserDTO: CreateUserDTO): Promise<UserDetailDTO> {
         const repository = new UserRepository();
         const sharedRepository = new UserSharedRepository();
+        const cacheRepository = new CacheRepository();
+
+        const cache = await cacheRepository.get('users');
+
+        if (!cache) {
+            const list = await sharedRepository.getUserAll();
+
+            cacheRepository.saveEx('users', list, 30);
+            console.log('criou cache');
+        }
+
+        if (cache) {
+            const exist = cache.filter((e: UserDetailDTO) => e.email === createUserDTO.email);
+
+            if (exist) {
+                if (exist) throw new CustomError('Email already exists from cache');
+            }
+        }
+
         const exists = await sharedRepository.getUserByEmail(createUserDTO.email);
 
         if (exists) throw new CustomError('Email already exists');
